cd7746d8f832428e55703ec2b5c1354389e5103a
Tathya <coding.tathya@gmail.com>
2025-06-26 10:01:33 +0530

    added reg link

---
+++ src/lib/components/reglink.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let href: string;
+  export let icon: string;
+  export let alt: string;
+</script>
+
+<div
+  class="w-1/4 aspect-square rounded-lg border-2 border-(--bg1) bg-(--bg1)/20 backdrop-blur-2xl"
+>
+  <a
+    class="flex flex-col items-center justify-center text-center w-full h-full py-2"
+    {href}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img class="w-full h-full mb-4 p-4" src={icon} {alt} />
+    <span class="text-lg font-semibold">{alt}</span>
+  </a>
+</div>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,4 +1,19 @@
-<section class="h-screen flex flex-col justify-center items-center">
-  <h1 class="w-full text-center text-8xl portrait:text-5xl">Register</h1>
-  <h2 class="w-full text-center text-3xl">Coming Soon!</h2>
+<script lang="ts">
+  import Reglink from "$lib/components/reglink.svelte";
+</script>
+
+<section class="h-full pb-12">
+  <h1 class="text-8xl text-center">Register</h1>
+  <div class="flex flex-row justify-around items-center w-screen h-full mt-12">
+    <Reglink
+      href={"https://forms.gle/23wcSJvQYCubwBTh9"}
+      icon={"/images/icons/individual.svg"}
+      alt={"Individual Registration"}
+    />
+    <Reglink
+      href={"https://forms.gle/NnjHAo3B1NCmF9mb8"}
+      icon={"/images/icons/group.svg"}
+      alt={"Delegation Registration"}
+    />
+  </div>
 </section>
